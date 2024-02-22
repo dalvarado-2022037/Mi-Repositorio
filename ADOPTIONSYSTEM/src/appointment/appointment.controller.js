c3fbd5e923293d7e3f1b1d0d39bcb00a7bb8b952
Douglas <dalvarado-2022037@kinal.edu.gt>
2024-02-22 16:16:51 -0600

    Se han realizados cambios de ADOPTIONS

--- ADOPTIONSYSTEM/src/appointment/appointment.controller.js
+++ ADOPTIONSYSTEM/src/appointment/appointment.controller.js
@@ -4,7 +4,13 @@ import Animal from '../animal/animal.model.js'
 import Appointment from '../appointment/appointment.model.js'
 
 export const save = async(req, res)=>{
-    try{
+    try{/*
+        let regex = /\d[2][/]/g
+        let regexAno = /\d[5][/]/g
+        let fecha = data.date
+        let fecha2 = fecha.replaceAll(regex, '')
+        console.log(fecha2)
+        console.log(fecha2.replaceAll(regexAno, ''))*/
         //Capturar la data
         let data = req.body
         data.user = req.user._id
@@ -14,18 +20,23 @@ export const save = async(req, res)=>{
         if(!animal) return res.status(404).send({message: 'Animal not found'})
         //Validar que la mascota no tenga una cita activa con esa persona <-
         //Validar si un animal ya tiene una cita o si un usuario ya tiene cita
+        //EJERCICIO <- Solo pueda tener una cita por dia
         let appointmentExist = await Appointment.findOne({
             $or: [
                 {
-                    animal: data.animal
+                    animal: data.animal,
+                    user: data.user
                 },
                 {
+                    date: data.date,
                     user: data.user
                 }
             ]
         })
+        if(animal == data.animal && user == data.user
+            || date == data.date && user == data.user)return a 
         if(appointmentExist) return res.send({message: 'Appointment already exist'})
-        //EJERCICIO <-
+        
         //Guardar
         let appointment = new Appointment(data)
         await appointment.save()
